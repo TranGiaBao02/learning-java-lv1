519a7842cf1dadd8a5d0ac07db3dbb61b08cc4c7
trangiabao <trangiabao270802@gmail.com>
2023-09-03 19:27:41 +0700

    a

--- src/java.java
+++ src/java.java
@@ -2,42 +2,41 @@ import java.util.Scanner;
 
 public class java {
     public static void main(String[] args) {
-        int n;
+        int m,y;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Nhập vào một số 1 -> 8: ");
-        n = sc.nextInt();
-        switch(n) {
-        case 1:{
-        	System.out.println("Thứ 2");
+        System.out.println("Nhập vào tháng 1 -> 12: ");
+        m = sc.nextInt();
+        System.out.println("Nhập vào năm: ");
+        y = sc.nextInt();
+        switch(m)
+        {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:{
+        	System.out.println("Có 31 ngày");
         	break;
         	}
         case 2:{
-        	System.out.println("Thứ 3");
-        	break;
+        	if(y%400 == 0 || y%4 == 0 && y%100 == 0) {
+        		System.out.println("Có 29 ngày");
         	}
-        case 3:{
-        	System.out.println("Thứ 4");
-        	break;
+        	else {
+        		System.out.println("Có 28 ngày");
         	}
-        case 4:{
-        	System.out.println("Thứ 5");
         	break;
-        	}
-        case 5: {
-        	System.out.println("Thứ 6");
-        	break;
-        	}
-        case 6:{
-        	System.out.println("Thứ 7");
-        	break;
-        	}
-        case 7:{
-        	System.out.println("CN");
-        	break;
-        	}
-        case 8:{
+        }
+        case 4:
+        case 6:
+        case 9:
+        case 11:{
+        	System.out.println("Có 30 ngày");
         	break;
         	}
         }
+        
     }
 }
