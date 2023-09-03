155d1fc5de5825eeb7e0a4489fb20eb64e3fb280
trangiabao <trangiabao270802@gmail.com>
2023-09-03 18:23:13 +0700

    a

--- src/java.java
+++ src/java.java
@@ -1,6 +1,43 @@
+import java.util.Scanner;
+
 public class java {
     public static void main(String[] args) {
-        // Mã chương trình của bạn ở đây
-        System.out.println("Xin chào");
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập vào một số 1 -> 8: ");
+        n = sc.nextInt();
+        switch(n) {
+        case 1:{
+        	System.out.println("Thứ 2");
+        	break;
+        	}
+        case 2:{
+        	System.out.println("Thứ 3");
+        	break;
+        	}
+        case 3:{
+        	System.out.println("Thứ 4");
+        	break;
+        	}
+        case 4:{
+        	System.out.println("Thứ 5");
+        	break;
+        	}
+        case 5: {
+        	System.out.println("Thứ 6");
+        	break;
+        	}
+        case 6:{
+        	System.out.println("Thứ 7");
+        	break;
+        	}
+        case 7:{
+        	System.out.println("CN");
+        	break;
+        	}
+        case 8:{
+        	break;
+        	}
+        }
     }
 }
